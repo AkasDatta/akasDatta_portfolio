1beb987d848e24295c4cdeae27eb9cfb61707741
Akas_Datta <012akasdatta@gmail.com>
2023-06-28 02:25:00 +0600

    modified footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Home/Shared/Navbar/Navbar";
+import Footer from "../components/Home/Shared/Footer/Footer";
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ import { router } from './Routes/Router';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <div className='max-w-screen-xl mx-auto px-5'>
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
