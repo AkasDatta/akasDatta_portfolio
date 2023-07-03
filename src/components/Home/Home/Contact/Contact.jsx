fd9d1c1b1bd83ee8ecf619fcde9526340b82c8d4
Akas_Datta <012akasdatta@gmail.com>
2023-07-03 22:31:37 +0600

    contact component modified and form added

--- src/components/Home/Home/Contact/Contact.jsx
+++ src/components/Home/Home/Contact/Contact.jsx
@@ -1,5 +1,7 @@
 import { IoMailOutline, IoLocationOutline } from 'react-icons/io5';
 import { FiPhone } from 'react-icons/fi';
+import image from "../../../../assets/image.png"
+import './Contact.css';
 
 const Contact = () => {
     return (
@@ -8,7 +10,6 @@ const Contact = () => {
             <div className="grid grid-cols-1 lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-1 gap-4">
             <div className="card pt-6">
               <div className="card-body rounded-3xl">
-                <h5 className="card-title">MORE ABOUT ME</h5>
                     <div className="col-span-12 md:col-span-12 lg:col-span-3 pb-7">
                     <div className="card ">
                         <div className="grid grid-cols-5 card-body rounded-3xl">
@@ -51,11 +52,35 @@ const Contact = () => {
             </div>
 
             <div className="card pt-6">
-              <div className="card-body bg-[#161616] rounded-3xl">
-                {/* <img className="mb-5 rounded-2xl" src={aboutMe2} alt="" /> */}
-                <h5 className="card-title">SHOWCASE</h5>
-                <p>Projects</p>
-              </div>
+                <div className="card-body bg-[#161616] rounded-3xl">
+                    <div className="card pt-0">
+                        <div className="card-body bg-[#161616] rounded-3xl">
+                            <div className='grid grid-cols-4'>
+                                <h2 className="text-4xl card-title col-span-3">Let's work <span className="text-sky-700">Together</span></h2>
+                                <img className="object-cover rounded-3xl w-full h-auto md:w-52 col-span-1" src={image} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="form-control py-5">
+                            <input type="text" placeholder="Name *" className="input input-bordered bg-gradient-to-r from-[#2A2A2A] to-[#191919]" />
+                        </div>
+                        <div className="form-control pb-5">
+                            <input type="email" placeholder="Email *" className="input input-bordered bg-gradient-to-r from-[#2A2A2A] to-[#191919]" />
+                        </div>
+                        <div>
+                            <textarea
+                            className="w-full rounded-lg input-bordered bg-gradient-to-r from-[#2A2A2A] to-[#191919] p-4"
+                            placeholder="Your Message *"
+                            rows="4"
+                            id="message"
+                            ></textarea>
+                        </div>
+                        <div className="form-control pt-3">
+                            <button className="btn btn-active">Send Message</button>
+                        </div>
+                    </div>
+                </div>
             </div>
           </div>
         </div>
