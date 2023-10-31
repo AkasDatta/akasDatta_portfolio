165d09c0e34d3d04a5a8e2a3803d0aed14f55f5d
Akas_Datta <012akasdatta@gmail.com>
2023-11-01 00:57:51 +0600

    tab added vertically

--- src/components/Home/Home/Experience/Experience.jsx
+++ src/components/Home/Home/Experience/Experience.jsx
@@ -1,4 +1,15 @@
-import React, { useState } from 'react';
+// const Experience = () => {
+//     return (
+//         <div>
+//             <h5 className="card-title font-bold text-3xl mb-4 px-8 pt-4 md:p-8 lg:p-8">EXPERIENCE</h5>
+//         </div>
+//     );
+// };
+
+// export default Experience;
+
+
+import { useState } from 'react';
 import './Experience.css'; // Create a CSS file for styling
 
 const VerticalTabs = () => {
