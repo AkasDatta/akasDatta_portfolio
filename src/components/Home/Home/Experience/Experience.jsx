c6d5c41699299a0fbefac6c48f9c9eeeb4fca816
Akas_Datta <012akasdatta@gmail.com>
2023-11-01 01:10:27 +0600

    tab position added

--- src/components/Home/Home/Experience/Experience.jsx
+++ src/components/Home/Home/Experience/Experience.jsx
@@ -1,44 +1,35 @@
-// const Experience = () => {
-//     return (
-//         <div>
-//             <h5 className="card-title font-bold text-3xl mb-4 px-8 pt-4 md:p-8 lg:p-8">EXPERIENCE</h5>
-//         </div>
-//     );
-// };
-
-// export default Experience;
-
-
 import { useState } from 'react';
-import './Experience.css'; // Create a CSS file for styling
+import './Experience.css';
 
-const VerticalTabs = () => {
-  const [activeTab, setActiveTab] = useState(0);
+const Experience = () => {
+    const [activeTab, setActiveTab] = useState(0);
 
-  const handleTabClick = (index) => {
-    setActiveTab(index);
-  };
-
-  const tabs = ['Tab 1', 'Tab 2', 'Tab 3']; // Add your tab names here
-
-  return (
-    <div className="vertical-tabs-container">
-      <div className="vertical-tabs">
-        {tabs.map((tab, index) => (
-          <div
-            key={index}
-            className={`tab ${index === activeTab ? 'active' : ''}`}
-            onClick={() => handleTabClick(index)}
-          >
-            {tab}
-          </div>
-        ))}
-      </div>
-      <div className="tab-content">
-        <div className="content">{tabs[activeTab]} content</div>
-      </div>
-    </div>
-  );
+    const handleTabClick = (index) => {
+      setActiveTab(index);
+    };
+  
+    const tabs = ['Tab 1', 'Tab 2', 'Tab 3']; 
+    return (
+        <div>
+            <h5 className="card-title font-bold text-3xl mb-4 px-8 pt-4 md:p-8 lg:p-8">EXPERIENCE</h5>
+            <div className="vertical-tabs-container px-8 mb-4 lg:mx-80 md:mx-40">
+                <div className="vertical-tabs">
+                    {tabs.map((tab, index) => (
+                    <div
+                        key={index}
+                        className={`tab ${index === activeTab ? 'active' : ''}`}
+                        onClick={() => handleTabClick(index)}
+                    >
+                        {tab}
+                    </div>
+                    ))}
+                </div>
+                <div className="tab-content">
+                    <div className="content">{tabs[activeTab]} content</div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
-export default VerticalTabs;
+export default Experience;
