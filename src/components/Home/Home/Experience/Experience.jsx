6729868c553e694ed291351dc9af1839e577b7f9
Akas_Datta <012akasdatta@gmail.com>
2023-11-01 01:48:18 +0600

    some error solved

--- src/components/Home/Home/Experience/Experience.jsx
+++ src/components/Home/Home/Experience/Experience.jsx
@@ -2,34 +2,42 @@ import { useState } from 'react';
 import './Experience.css';
 
 const Experience = () => {
-    const [activeTab, setActiveTab] = useState(0);
+  const [activeTab, setActiveTab] = useState(null);
 
-    const handleTabClick = (index) => {
-      setActiveTab(index);
-    };
-  
-    const tabs = ['Tab 1', 'Tab 2', 'Tab 3']; 
-    return (
-        <div>
-            <h5 className="card-title font-bold text-3xl mb-4 px-8 pt-4 md:p-8 lg:p-8">EXPERIENCE</h5>
-            <div className="vertical-tabs-container px-8 mb-4 lg:mx-80 md:mx-40">
-                <div className="vertical-tabs">
-                    {tabs.map((tab, index) => (
-                    <div
-                        key={index}
-                        className={`tab ${index === activeTab ? 'active' : ''}`}
-                        onClick={() => handleTabClick(index)}
-                    >
-                        {tab}
-                    </div>
-                    ))}
-                </div>
-                <div className="tab-content">
-                    <div className="content">{tabs[activeTab]} content</div>
-                </div>
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+  };
+
+  const tabs = ['MyNadezhda consultancy', 'Desh IT'];
+
+  return (
+    <div>
+        <h5 className="card-title font-bold text-3xl mb-4 px-8 pt-4 md:p-8 lg:p-8">EXPERIENCE</h5>
+        <div className="experience-container md:mx-40 lg:mx-80 px-8">
+            <div className="experience-sidebar">
+            {tabs.map((tab, index) => (
+            <div
+                key={index}
+                className={`tab ${index === activeTab ? 'active' : ''}`}
+                onClick={() => handleTabClick(index)}
+            >
+                {tab}
+                {index === activeTab && <hr className="active-line" />}
+            </div>
+            ))}
+        </div>
+        <div className="tab-content">
+            <div className="content">
+            {activeTab !== null ? tabs[activeTab] + ' content' : 'Select a tab'}
             </div>
         </div>
-    );
+        </div>
+    </div>
+  );
 };
 
 export default Experience;
+
+
+
+
