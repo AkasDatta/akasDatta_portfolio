b0d023c3ad6f60cd61edaf402c49c3498bd7441a
Akas_Datta <012akasdatta@gmail.com>
2023-11-01 02:11:37 +0600

    tab something changed

--- src/components/Home/Home/Experience/Experience.jsx
+++ src/components/Home/Home/Experience/Experience.jsx
@@ -12,32 +12,28 @@ const Experience = () => {
 
   return (
     <div>
-        <h5 className="card-title font-bold text-3xl mb-4 px-8 pt-4 md:p-8 lg:p-8">EXPERIENCE</h5>
-        <div className="experience-container md:mx-40 lg:mx-80 px-8">
-            <div className="experience-sidebar">
-            {tabs.map((tab, index) => (
+      <h5 className="card-title font-bold text-3xl mb-4 px-8 pt-4 md:p-8 lg:p-8">EXPERIENCE</h5>
+      <div className="experience-container md:mx-36 lg:mx-80 px-8">
+        <div className={`experience-sidebar ${activeTab !== null ? 'small-devices' : ''}`}>
+          {tabs.map((tab, index) => (
             <div
-                key={index}
-                className={`tab ${index === activeTab ? 'active' : ''}`}
-                onClick={() => handleTabClick(index)}
+              key={index}
+              className={`tab ${index === activeTab ? 'active' : ''}`}
+              onClick={() => handleTabClick(index)}
             >
-                {tab}
-                {index === activeTab && <hr className="active-line" />}
+              {tab}
+              {index === activeTab && <hr className="active-line" />}
             </div>
-            ))}
+          ))}
         </div>
-        <div className="tab-content">
-            <div className="content">
+        <div className={`tab-content ${activeTab !== null ? 'small-devices-content' : ''}`}>
+          <div className="content">
             {activeTab !== null ? tabs[activeTab] + ' content' : 'Select a tab'}
-            </div>
+          </div>
         </div>
-        </div>
+      </div>
     </div>
   );
 };
 
 export default Experience;
-
-
-
-
