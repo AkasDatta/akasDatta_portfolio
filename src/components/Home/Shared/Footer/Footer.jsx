869b897a8211fa626aac91dcadfe43248fac8122
Akas_Datta <012akasdatta@gmail.com>
2023-11-06 21:55:55 +0600

    footer responsive changed

--- src/components/Home/Shared/Footer/Footer.jsx
+++ src/components/Home/Shared/Footer/Footer.jsx
@@ -4,11 +4,11 @@ const Footer = () => {
         <div>
             <footer className="bg-black-800">
                 <div className="mx-auto max-w-5xl px-4 py-16 sm:px-6 lg:px-8">
-                    <div className="flex justify-center text-white font-bold text-4xl">
+                    <div className="flex justify-center text-white font-bold text-xl">
                         Akas Datta
                     </div>
 
-                    <ul className="mt-6 flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-12">
+                    <ul className="mt-6 flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-12 mx-10">
                         <li>
                             <a
                             className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
