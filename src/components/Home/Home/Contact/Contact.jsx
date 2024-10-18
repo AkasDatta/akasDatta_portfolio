eba1f66feb2aba89ec516045deaf6488682b2571
Akas_Datta <012akasdatta@gmail.com>
2024-10-19 00:03:15 +0600

    changes email path

--- src/components/Home/Home/Contact/Contact.jsx
+++ src/components/Home/Home/Contact/Contact.jsx
@@ -81,7 +81,7 @@ export const Contact = () => {
                     </div>
                     <form className="card" ref={form} onSubmit={sendEmail}>
                         <div className="form-control py-5">
-                            <input type="text" name=" {{from_name}}" placeholder="Name *" className="input input-bordered bg-gradient-to-r text-white from-[#2A2A2A] to-[#191919]" />
+                            <input type="text" name="from_name" placeholder="Name *" className="input input-bordered bg-gradient-to-r text-white from-[#2A2A2A] to-[#191919]" />
                         </div>
                         <div className="form-control pb-5">
                             <input type="email" name="email_id" placeholder="Email *" className="input input-bordered text-white bg-gradient-to-r from-[#2A2A2A] to-[#191919]" />
