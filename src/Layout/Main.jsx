4d229ee23766fcdbd7ff775d12acdd8ae5a8c398
Akas_Datta <012akasdatta@gmail.com>
2024-12-03 01:33:43 +0600

    adde gototop

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Home/Shared/Navbar/Navbar";
 import Footer from "../components/Home/Shared/Footer/Footer";
 import { useEffect, useState } from "react";
 import GoToTop from "../components/Home/Home/GoToTop/GoToTop";
+import { ThemeProvider } from "styled-components";
 
 const Main = () => {
     const [isVisible, setIsVisible] = useState(true);
@@ -13,16 +14,39 @@ const Main = () => {
         }, 1000);
         return () => clearTimeout(timeout);
     }, []);
+
+    const theme = {
+        colors: {
+          heading: "rgb(24 24 29)",
+          text: "rgb(24 24 29)",
+          white: "#fff",
+          black: " #212529",
+          helper: "#0369A1",
+          bg: "rgb(249 249 255)",
+          footer_bg: "#0a1435",
+          btn: "#0369A1",
+          border: "rgba(98, 84, 243, 0.5)",
+          hr: "#ffffff",
+          gradient:
+            "linear-gradient(0deg, rgb(132 144 255) 0%, rgb(98 189 252) 100%)",
+          shadow:
+            "rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;",
+          shadowSupport: " rgba(0, 0, 0, 0.16) 0px 1px 4px",
+        },
+        media: { mobile: "768px", tab: "998px" },
+      };
     
     return (
           <div>
             {isVisible ? <div className="flex justify-center items-center h-screen">
                 <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-blue-500"></div>
                 </div> : <div>
+                    <ThemeProvider theme={theme}>
                         <Navbar></Navbar>
                         <Outlet></Outlet>
                         <GoToTop></GoToTop>
                         <Footer></Footer>
+                    </ThemeProvider>
                 </div> }
             </div>
     );
--- src/components/Home/Home/GoToTop/GoToTop.jsx
+++ src/components/Home/Home/GoToTop/GoToTop.jsx
@@ -1,14 +1,85 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { FaArrowUp } from "react-icons/fa";
 
 const GoToTop = () => {
-    const goToBtn = () => {
-        window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  const [isVisible, setIsVisible] = useState(false);
+
+  const goToBtn = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+
+  const listenToScroll = () => {
+    let heightToHidden = 20;
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    if (winScroll > heightToHidden) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
     }
-    return (
-        <div>
-            <button className='top-btn' onClick={goToBtn}> ttt</button>
-           
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+    return () => window.removeEventListener("scroll", listenToScroll);
+  }, []);
+
+  return (
+    <Wrapper>
+      {isVisible && (
+        <div className="top-btn" onClick={goToBtn}>
+          <FaArrowUp className="top-btn--icon" />
         </div>
-    );
+      )}
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  .top-btn {
+    font-size: 1.2rem;
+    width: 3rem;
+    height: 3rem;
+    color: #fff;
+    background-color: ${({ theme }) => theme.colors.btn};
+    box-shadow: ${({ theme }) => theme.colors.shadow};
+    border-radius: 50%;
+    position: fixed;
+    bottom: 4rem;
+    right: 5rem;
+    z-index: 999;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+
+    &--icon {
+      animation: gototop 1.2s linear infinite alternate-reverse;
+    }
+
+    @keyframes gototop {
+      0% {
+        transform: translateY(-0.4rem);
+      }
+      100% {
+        transform: translateY(0.2rem);
+      }
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    .top-btn {
+      right: 0;
+      left: 80%;
+    }
+  }
+`;
 
 export default GoToTop;
